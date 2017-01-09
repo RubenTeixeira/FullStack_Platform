f5c83d7190eda3451aebb80569cb57497640056e
Gil Vieira <1140984@isep.ipp.pt>
2017-01-09 10:29:43 +0000

    DTO caminho

---
+++ PortoVisitas/ClassLibrary/DTO/CaminhoDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.DTO
+{
+    public class CaminhoDTO
+    {
+        public int ID { get; set; }
+
+        public int poiOrigID { get; set; }
+
+        public int poiDestID { get; set; }
+
+        public int inclinacao { get; set; }
+    }
+}
---
+++ PortoVisitas/ClassLibrary/DTO/DecisionHelperDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.DTO
+{
+    public class DecisionHelperDTO
+    {
+        public int poiOrigem { get; set; }
+
+        public List<int> listaPOIs { get; set; }
+
+        public int inclinacao { get; set; }
+
+        public int minutoInicial { get; set; }
+
+        public String tipoTransporte { get; set; }
+    }
+}
