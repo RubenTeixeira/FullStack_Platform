24350fa99ebd9f2eb9280786107cecf3dba178ef
Gil Vieira <1140984@isep.ipp.pt>
2017-01-10 12:07:47 +0000

    Algav connection -> Read pois and caminhos Run algav

--- PortoVisitas/ALGAVAPI/Controllers/DecisionHelperController.cs
+++ PortoVisitas/ALGAVAPI/Controllers/DecisionHelperController.cs
@@ -3,6 +3,7 @@ using ClassLibrary.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -14,18 +15,82 @@ namespace ALGAVAPI.Controllers
 {
     public class DecisionHelperController : ApiController
     {
-        public Task<IHttpActionResult> pois;
         public HttpClient client;
 
         [System.Web.Http.HttpGet]
         [System.Web.Http.Route("api/Test")]
-        public void Test()
-        {   
+        public async Task<IHttpActionResult> Test()
+        {
             // Get all pois
-            //pois = GetPOIs();
-            System.IO.File.WriteAllText(@"C:\Users\Gil\Documents\IISExpress\pois.txt", pois.ToString());
-
+            List<POIDTO> pois = await GetPOIs();
             // Get caminhos
+            List<CaminhoDTO> caminhos = await GetCaminhos();
+
+
+            // Escreve base de conhecimento
+            using (System.IO.StreamWriter file =
+            new System.IO.StreamWriter(@"C:\Users\Gil\Documents\IISExpress\knowledge.pl"))
+
+            foreach (POIDTO poidto in pois)
+                {
+                    string id = poidto.ID.ToString();
+                    string horaAberturaHora = poidto.OpenHour.Hour.ToString();
+                    string horaAberturaMinutos = poidto.OpenHour.Minute.ToString();
+                    string horaAberturaSegundos = "0";
+                    string horaFechoHora = poidto.CloseHour.Hour.ToString();
+                    string horaFechoMinutos = poidto.CloseHour.Minute.ToString();
+                    string horaFechoSegundos = "0";
+                    string tempoEstimadoVisita = "60";
+                    string poiLatitude = poidto.GPS_Lat.ToString();
+                    poiLatitude = poiLatitude.Replace(',', '.');
+                    string poiLongitude = poidto.GPS_Long.ToString();
+                    poiLongitude = poiLongitude.Replace(',', '.');
+
+                    file.WriteLine("poi("+id+","+"time("+horaAberturaHora+","+horaAberturaMinutos+","+
+                                    horaAberturaSegundos+")"+","+
+                                    "time("+horaFechoHora+", "+horaFechoMinutos+", "+
+                                    horaFechoSegundos + ")" + ","
+                                    + tempoEstimadoVisita+","+poiLatitude+","+poiLongitude+").");
+                }
+
+            int idCaminho = 0;
+            using (System.IO.StreamWriter file =
+            new System.IO.StreamWriter(@"C:\Users\Gil\Documents\IISExpress\knowledge.pl", true))
+                foreach (CaminhoDTO caminhoDto in caminhos)
+            {
+                string idPoiInicio = caminhoDto.POIID.ToString();
+                string idPoiFinal = caminhoDto.ConnectedPOIID.ToString();
+                    int poiInicialID = caminhoDto.POIID;
+                    int poiFinalID = caminhoDto.ConnectedPOIID;
+
+                    POIDTO poiInicial = null;
+                    POIDTO poiFinal = null;
+                    foreach(POIDTO poi in pois)
+                    {
+                        if(poi.ID == poiInicialID) { poiInicial = poi; }
+                        if(poi.ID == poiFinalID) { poiFinal = poi; }
+                    }
+
+                    Debug.Write(poiInicialID);
+                    Debug.Write(poiInicial.ID);
+                        double poiFinal_Lat = (double)poiFinal.GPS_Lat;
+                        double poiFinal_Long = (double)poiFinal.GPS_Long;
+                        double poiInicial_Lat = (double)poiInicial.GPS_Lat;
+                        double poiInicial_Long = (double)poiInicial.GPS_Long;
+
+
+                    double distanciaEntrePois = Math.Sqrt((poiFinal_Lat - poiInicial_Lat) * (poiFinal_Lat - poiInicial_Lat) + (poiFinal_Long - poiInicial_Long)*(poiFinal_Long - poiInicial_Long));
+
+                    double inclinacao = ((poiFinal.Altitude - poiInicial.Altitude) * 100) / distanciaEntrePois;
+
+                file.WriteLine("caminho("+idCaminho+","+idPoiInicio+","+idPoiFinal+","+inclinacao+").");
+                idCaminho++;
+            }
+
+            File.AppendAllText(@"C:\Users\Gil\Documents\IISExpress\knowledge.pl", "transporte(pe, 50)."+ System.Environment.NewLine);
+            File.AppendAllText(@"C:\Users\Gil\Documents\IISExpress\knowledge.pl", "transporte(carro, 300)."+ System.Environment.NewLine);
+            File.AppendAllText(@"C:\Users\Gil\Documents\IISExpress\knowledge.pl", "transporte(autocarro, 200)."+ System.Environment.NewLine);
+            File.AppendAllText(@"C:\Users\Gil\Documents\IISExpress\knowledge.pl", "transporte(tuk, 100).");
 
 
             System.Diagnostics.Process process = new System.Diagnostics.Process();
@@ -37,22 +102,40 @@ namespace ALGAVAPI.Controllers
             process.Start();
 
             // Read result 1 and 2
+
+            return Ok(pois);
         }
 
-        //public async Task<List<POIDTO>> GetPOIs()
-        //{
-        //    client = DBWebApiHttpClient.GetClient();
+        public async Task<List<POIDTO>> GetPOIs()
+        {
+            client = DBWebApiHttpClient.GetClient();
 
-        //    List<POIDTO> pois = null;
-        //    var response = await client.GetAsync("api/POI/");
+            List<POIDTO> pois = null;
+            var response = await client.GetAsync("api/POI/");
 
-        //    if (response.IsSuccessStatusCode)
-        //    {
-        //        pois = await response.Content.ReadAsAsync<List<POIDTO>>();
-        //        return Ok(pois);
-        //    }
+            if (response.IsSuccessStatusCode)
+            {
+                pois = (List<POIDTO>) await response.Content.ReadAsAsync<IEnumerable<POIDTO>>();
+                return pois;
+            }
 
-        //    return null;
-        //}
+            return null;
+        }
+
+        public async Task<List<CaminhoDTO>> GetCaminhos()
+        {
+            client = DBWebApiHttpClient.GetClient();
+
+            List<CaminhoDTO> caminhos = null;
+            var response = await client.GetAsync("api/Caminho/");
+
+            if (response.IsSuccessStatusCode)
+            {
+                caminhos = (List<CaminhoDTO>)await response.Content.ReadAsAsync<IEnumerable<CaminhoDTO>>();
+                return caminhos;
+            }
+
+            return null;
+        }
     }
 }
