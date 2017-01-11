98fa1c1bf14c3c86a4771968210e2619c939554a
Marcelo-Oliveira <1141061@isep.ipp.pt>
2017-01-11 15:45:28 +0000

    Adição de DTO para pedido de ALGAV

---
+++ PortoVisitas/ClassLibrary/DTO/Algav1DTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.DTO
+{
+    public class Algav1DTO
+    {
+        public int poiOrigem { get; set; }
+        public List<int> poiList { get; set; }
+        [DisplayFormat(DataFormatString = "{0:HH:mm}")]
+        [DataType(DataType.Time)]
+        public DateTime startingMinute { get; set; }
+        public int inclinacaoMax { get; set; }
+        public string tipoVeiculo { get; set; }
+        public int kilometrosMax { get; set; }
+    }
+}
---
+++ PortoVisitas/ClassLibrary/DTO/Algav2DTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.DTO
+{
+    public class Algav2DTO
+    {
+        public int poiOrigem { get; set; }
+        public int maxHorasVisita { get; set; }
+        [DisplayFormat(DataFormatString = "{0:HH:mm}")]
+        [DataType(DataType.Time)]
+        public DateTime horaInicialVisita { get; set; }
+        public int inclinacaoMax { get; set; }
+        public string tipoVeiculo { get; set; }
+        public int kilometrosMax { get; set; }
+    }
+}
